fa79bc13d18f190ccdd4a7200d97c4035a4fb7df
jimmyto-09 <jimmy.acastilloa@gmail.com>
2026-05-10 01:16:19 +0200

    Refactor ACM certificate and Route 53 resources

--- acm.tf
+++ acm.tf
@@ -34,14 +34,3 @@ resource "aws_acm_certificate_validation" "cert" {
     for r in aws_route53_record.cert_validation : r.fqdn
   ]
 }
-#resource "aws_lb_listener" "https" {
-#  load_balancer_arn = aws_lb.alb.arn
-#  port              = 443
-#  protocol          = "HTTPS"
-#  ssl_policy        = "ELBSecurityPolicy-2016-08"
-#  certificate_arn   = data.aws_acm_certificate.cert.arn
-# default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.tg.arn
-#  }
-#}
